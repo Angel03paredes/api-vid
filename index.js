7c8d78da825b316f2404795f335602f9511dea5c
Angel03paredes <angel.2899pasj@gmail.com>
2022-05-10 22:58:23 -0500

    image xd

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ const app = express();
 require("./db")()
 
 app.use(express.json())
+
 app.use("/user",userRoutes)
 app.use("/inventory",inventoryRoutes)
+
+app.get("/",(req,res)=>{
+    res.send("<img src='https://cdn.discordapp.com/attachments/883411559601274981/973410858220798033/unknown.png'  />")
+})
 
 app.listen(process.env.PORT || 3000,()=>{
     console.log("Listening on port 3000" );
